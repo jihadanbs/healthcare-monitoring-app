f5073a9d4d03f6c1c32c78b47e22925eab3a837b
Jihadan Beckhianosyuhada <jihadanbs11@gmail.com>
2024-12-19 03:41:31 +0700

    [EXPERIMENTAL] menghapus dan merapikan code models

--- backend/src/models/Appointment.js
+++ backend/src/models/Appointment.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 
 const AppointmentSchema = new mongoose.Schema({
-    patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    doctorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    date: { type: Date, required: true },
-    status: { type: String, enum: ['Pending', 'Confirmed', 'Completed'], default: 'Pending' }
+    patientId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    doctorId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    date: { 
+        type: Date, 
+        required: true 
+    },
+    status: { 
+        type: String, 
+        enum: ['Pending', 'Confirmed', 'Completed'], 
+        default: 'Pending' 
+    }
 });
 
 module.exports = mongoose.model('Appointment', AppointmentSchema);
